7f5e4915a5eb68eae6f2163dee58e1d4c18bf73e
Jhonatan Mejias <jhonatanmejias@gmail.com>
2022-05-24 13:24:21 -0400

    Anavnces

--- wp-content/themes/WP_bulma_theme/footer.php
+++ wp-content/themes/WP_bulma_theme/footer.php
@@ -1,18 +1,34 @@
-<footer class="footer" style="padding: 2em;">
-	<div class="container">
-		<div class="columns">
-			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-1' ); ?></div>
-			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-2' ); ?></div>
-			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-3' ); ?></div>
-			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-4' ); ?></div>
-		</div>
+<footer >
+<section class="hero has-background-grey-2 has-text-white">
+      <div class="hero-body">
 
-		<div class="columns">
-			<div class="column">
-				<div><?=get_theme_mod( 'footer_copyright_text' )?></div>
-			</div>
-		</div>
-	</div>
+            <div class="columns  is-centered pt-40 is-vcentered">
+               <div class="column is-8 has-text-centered   has-text-centered-mobile is-12-mobile">
+                 <p class="is-size-10"> * Se deja constancia que todas las imágenes, tipologías, superficies, 
+                    número de viviendas, arquitectura, ubicaciones y etapas del proyecto 
+                    que se exhiben en la publicidad son de carácter estrictamente preliminar
+                     y obedecen a la presentación de un proyecto en blanco, reservándose 
+                     expresamente la compañía la facultad y el derecho de modificarlas y/o
+                      rediseñarlas en los términos que estime pertinente, y según dispongan 
+                      los antecedentes legales del mismo.
+                  </p>
+               </div>
+            </div>
+
+            <div class="columns is-centered is-vcentered">
+               <div class="column has-text-centered">
+                  <p class="is-size-7 espacio4px f_mb"><img class="alinea_img" width="15px"
+                        src="<?php echo get_template_directory_uri();?>/assets/img/pin.png" alt="pin"> CAPITÁN ROBERTO PÉREZ 0704, IQUIQUE <span
+                        class="is-hidden-mobile">-</span><br class="is-hidden-tablet"><img class="alinea_img"
+                        width="15px" src="<?php echo get_template_directory_uri();?>/assets/img/fono.png" alt="fono"> CONTACTO COMERCIAL: +56 9 9334 7004
+                     <span class="is-hidden-mobile">-</span><br class="is-hidden-tablet"><img class="alinea_img"
+                        width="15px" src="<?php echo get_template_directory_uri();?>/assets/img/mail.png"> dalipeninsula@loga.cl
+                  </p>
+               </div>
+            </div>
+      
+      </div>
+</section>
 </footer>
 
 <?php wp_footer(); ?>
