58e7102f0eb736a23b2088a6f1e364ca3bb62d72
Jhonatan Mejias <jhonatanmejias@gmail.com>
2022-05-25 15:34:56 -0400

    pryecto

--- wp-content/themes/WP_bulma_theme/template-proyecto.php
+++ wp-content/themes/WP_bulma_theme/template-proyecto.php
@@ -16,30 +16,142 @@
 <?php get_header(); ?>
 
 
-<section id="header-diseño" class="fondo-header ">
-      <div class="hero-body">
+<!-- SECCION 6 PERSPECTVA -->
+<section id="header-proyecto">
+    <div class="hero-body" >
+        <div class="container">
+            <div class="columns is-vcentered">
+                <div class="column is-5 is-offset-6-desktop has-text-white has-text-right title_box_perspectiva ">
+                    <p class="f_bozon_r pt-full-50 interlineado-1">Hemos creado una experiencia diferente en el mejor sector de Cavancha. 
+                        Porque Dalí Península es lo nuevo en diseño y arquitectura, ideal para
+                         las personas que buscan lo mejor para su día a día.<p>
 
-            <div class="columns is-marginless is-vcentered pt-5p pr-5p ">
-               <div class="column is-offset-6-desktop is-6-desktop is-8-tablet is-11-mobile is-offset-1-mobile ">
+                    <h2 class="f_mb is-size-3 ">EXCLUSIVIDAD</h2>
+                    <h3 class="f_ml">DISEÑO</h3>
+                    <h3 class="f_ml">MODERNIDAD</h3>
             
-               </div>
-            </div>
-     
+                    <h3 class="f_mmI">TODO A TUS PIES</h3>
+                </div>
+
          
-      </div>
-    
+            </div>
+        </div>
+    </div>
 </section>
 
-<section id="cocina" >
-      <div class="hero-foot has-background-black">
-            <div class="columns is-marginless is-vcentered pt-5p pr-5p ">
-               <div class="column is-offset-3-desktop is-6-desktop is-8-tablet is-10-mobile is-offset-1-mobile ">
-                    <p>s que tu experiencia de compra sea perfectasta nueva etapa tal y como soñaste. Conoce los beneficios 
-                    y alianzas con algunos de nuestros proyectos y aprovecha las oportunidades que tenemos para ti.</p>
 
-               </div>
+<!-- SECCION 2 DISEÑO-->
+<section class="hero has-background-black">
+    <div class="hero-body ">
+        <div class="container">
+
+            <div class="columns has-text-white">
+
+                <div class="column is-5-desktop is-offset-2-desktop ">
+                    <h2 class="f_ml is-size-4 espacio3">EL DISEÑO Y LA VANGUARDIA EN UN SOLO LUGAR</h2>
+                    <img  src="<?php echo get_template_directory_uri();?>/assets/img/line_white.png" alt="line">
+                    <p class="has-text-justified">Respira hondo, siente la desconexión y disfruta del relajo que solo te pueden dar las olas de
+                         Iquique. <br> <br> Edificio Dalí Península es un proyecto boutique que cuenta con 16 pisos y tan sólo
+                          42 departamentos. Ubicado frente a la costa del Pacífico, vas a experimentar la tranquilidad
+                           y desconexión a otro nivel.</p>
+                </div>
+                <div class="column is-5-desktop is-offset-1-desktop pt-40 ">
+                    <h2 class="f_neutra">2, 3 & 4 <br>  Dormitorios.</h2>
+                    <img src="<?php echo get_template_directory_uri();?>/assets/img/line_blue.png" alt="line">
+
+                    <h2 class="f_neutra">Penthouses</h2>
+                </div>
             </div>
-      </div>
+
+           
+        </div>
+    </div>
+</section>
+
+
+<!-- SECCION 4 PISOS -->
+<section id="pisos"  class="has-background-white">
+    <div class="hero-body" >
+        <div class="container">
+            <div class="columns is-vcentered has-text-centered ">
+                <div class="column is-three-fifths is-offset-one-fifth has-text-white  has-text-centered ">
+
+                    <div class="columns  has-text-centered">
+                        <div class="column is-3  is-offset-2 has-text-white  ">
+                            <button class="button-blue">Piso 2 al 13</button>
+                        </div>
+                        <div class="column is-3 has-text-white  ">
+                            <button class="button-gray">Piso 14 y 15*</button>
+                        </div>
+                        <div class="column is-3 has-text-white  ">
+                            <button class="button-gray">Penthouses</button>
+                        </div>
+
+                    </div>
+
+
+                    <div class="columns is-vcentered has-text-centered">
+                        <div class="column has-text-white  ">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/piso_2_13.png" alt="play">
+                        </div>
+
+                    </div>
+
+                    <div class="columns is-vcentered has-text-centered">
+                        <div class="column  has-text-white  ">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/brand_pisos.png" alt="play">
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<!-- SECCION 4 MAPA -->
+<section id="pisos"  class="has-background-white">
+    <div class="hero-body" >
+        <div class="container">
+            <div class="columns is-vcentered has-text-centered ">
+                <div class="column is-three-fifths is-offset-one-fifth has-text-white  has-text-centered ">
+
+                    <div class="columns  has-text-centered">
+                        <div class="column is-3  is-offset-2 has-text-white  ">
+                            <button class="button-blue">Piso 2 al 13</button>
+                        </div>
+                        <div class="column is-3 has-text-white  ">
+                            <button class="button-gray">Piso 14 y 15*</button>
+                        </div>
+                        <div class="column is-3 has-text-white  ">
+                            <button class="button-gray">Penthouses</button>
+                        </div>
+
+                    </div>
+
+
+                    <div class="columns is-vcentered has-text-centered">
+                        <div class="column has-text-white  ">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/piso_2_13.png" alt="play">
+                        </div>
+
+                    </div>
+
+                    <div class="columns is-vcentered has-text-centered">
+                        <div class="column  has-text-white  ">
+                        <img src="<?php echo get_template_directory_uri();?>/assets/img/brand_pisos.png" alt="play">
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
 </section>
 
 
